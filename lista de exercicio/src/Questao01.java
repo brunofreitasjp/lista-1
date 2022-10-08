c34e5626021fa72b10b76988a7cd7c6ab6921322
Bruno Silva de Freitas <62038099+brunofreitasjp@users.noreply.github.com>
2022-10-07 23:42:52 -0300

    Add files via upload

--- lista de exercicio/src/Questao01.java
+++ lista de exercicio/src/Questao01.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Questao01 {
+    public static void main(String[] args) {
+        System.out.println("digite os creditos galaticos ");
+
+        Scanner leitor = new Scanner(System.in);
+        float credito = leitor.nextFloat();
+
+        System.out.println("digite a porcentagem do aumento");
+        float porccentagem = leitor.nextFloat();
+
+        float result = credito + ((porccentagem / 100) * credito);
+
+        System.out.println("o valor com o aumento e de : " + result);
+    }
 }
--- lista de exercicio/src/Questao04.java
+++ lista de exercicio/src/Questao04.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Questao04 {
-    //
+    public static void main(String[] args) {
+
+        System.out.println("digite a quantidade de litros que foi colocado ");
+
+        Scanner leitor = new Scanner(System.in);
+        float litros = leitor.nextFloat();
+
+        float gasolina = (float) (litros * 2.50);
+
+        System.out.println("o valor do combustivel a ser pago e de :" + gasolina);
+
+        float refrigerante = 6;
+
+        float total = gasolina + refrigerante;
+        System.out.println("o valor total a ser pago e de : " + total);
+    }
 }
